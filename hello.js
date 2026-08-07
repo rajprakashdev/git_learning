0de6cef2972846a9cbf3fe80060098154ca02305
prakashrajsevugan <prakashraj8739@gmail.com>
2026-08-07 21:25:52 +0530

    Revert "Revert "adding pay and submit func""
    This reverts commit de6526308a079377b4d64736de34f61db3b9ecc6.

--- hello.js
+++ hello.js
@@ -2,7 +2,6 @@ console.log("Hello, Friends!");
 
 console.log("How are you?");
 
-
 console.log("I'm fine, thank you!");
 
 const submitButton = () =>
@@ -10,15 +9,18 @@ const submitButton = () =>
     console.log("Submit button clicked!");
 }
 
+const pay = () =>{
+    console.log("Payment successful!");
+}
+
 console.log("Thank you for your purchase!");
 
 submitButton();
+
 pay();
 
 function pay() {
     console.log("Payment processed successfully!");
 }
 
-function cancel() {
-    console.log("Payment canceled.");
-}
+
